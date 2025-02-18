1551172b3444c33dfe993182133084e70cabd7c8
muchoki769 <ndungudavidmuchoki@gmail.com>
2025-02-18 17:57:03 +0300

    corrected a auth

--- auth.ts
+++ auth.ts
@@ -3,31 +3,43 @@ import {authConfig} from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import {z} from 'zod';
 import { pool } from 'dbConfig';
+import postgres from 'postgres';
 import type {User} from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 // import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
-async function getUser(email:string): Promise<User | undefined> {
-    try{
-        const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
-        // if (result.rows.length >0) {
-        //     return result.rows[0];
-        // }else{
-        //     return undefined;
-        // }
-        return result.rows.length > 0 ? result.rows[0] : undefined;
-        // const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-        // return user[0];
-        // const user = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
-        // return user.rows[0];
 
-    }catch (error){
-        console.error('Failed to fetch user:',error);
-        throw new Error('Failed to fetch user');
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+      const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+      return user[0];
+    } catch (error) {
+      console.error('Failed to fetch user:', error);
+      throw new Error('Failed to fetch user.');
     }
-}
+  }
+
+// async function getUser(email:string): Promise<User | undefined> {
+//     try{
+//         const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+//         // if (result.rows.length >0) {
+//         //     return result.rows[0];
+//         // }else{
+//         //     return undefined;
+//         // }
+//         return result.rows.length > 0 ? result.rows[0] : undefined;
+//         // const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+//         // return user[0];
+//         // const user = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+//         // return user.rows[0];
+
+//     }catch (error){
+//         console.error('Failed to fetch user:',error);
+//         throw new Error('Failed to fetch user');
+//     }
+// }
 
 export const {auth, signIn, signOut}=NextAuth({
     ...authConfig,
